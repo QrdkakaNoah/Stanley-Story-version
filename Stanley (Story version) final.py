7cdc621fda551908dcf0e205682fc2ae9b1e151b
QrdkakaNoah <46796628+QrdkakaNoah@users.noreply.github.com>
2019-01-30 14:37:53 -0700

    Add files via upload

---
+++ Stanley (Story version) final.py
@@ -0,0 +1,180 @@
+###
+#Noah Gaviña
+#1.18.19
+#Employee #427 simulator Story edition final
+###
+print("Credit to Davey Wreden for this story.")
+print("Gane made by Noah Gaviña.")
+while True:
+    import random
+    import string
+    random.choice(string.ascii_lowercase)
+    print("This is a very sad story about the death of a man named Stanley.")
+    print("Hello Employee #427.")
+    y=random.choice(string.ascii_lowercase)
+    print("Press the " +y+ " key.")
+    x=input()
+    if x == y:
+        import random
+        import string
+        random.choice(string.ascii_lowercase)
+        print("Stanley is quite a boring fellow. He has a job that demands nothing of him, and every button that he pushes is a reminder of the inconsequential nature of his existence.")
+        print("Look at him there, pushing buttons, doing exactly what he's told to do. Now, he's pushing a button. Now, he's eating lunch. Now, he's going home. Now, he's coming back to work.")
+        y=random.choice(string.ascii_lowercase)
+        print("Press the " +y+ " key.")
+        x=input()
+        if x == y:
+            import random
+            import string
+            random.choice(string.ascii_lowercase)
+            print("One might even feel sorry for him, except that he's chosen this life.")
+            print("But in his mind, ah, in his mind he can go on fantastic adventures!")
+            print("From behind his desk, Stanley dreamed of wild expeditions into the unknown, fantastic discoveries of new lands! It was wonderful!")
+            y=random.choice(string.ascii_lowercase)
+            print("Press the " +y+ " key.")
+            x=input()
+            if x == y:
+                import random
+                import string
+                random.choice(string.ascii_lowercase)
+                print("And each day that he returned to work was a reminder that none of it would ever happen to him.")
+                print("And so he began to fantasize about his own job. First, he imagined that one day while at work, he stepped up from his desk to realize that all of his co-workers, his boss, everyone in the building had suddenly vanished off the face of the earth. The thought excited him terribly.")
+                print("So, he went further. He imagined that he came to two open doors and that he could through either. At last! Choice!")
+                y=random.choice(string.ascii_lowercase)
+                print("Press the " +y+ " key.")
+                x=input()
+                if x == y:
+                    import random
+                    import string
+                    random.choice(string.ascii_lowercase)
+                    print("It barely even mattered what lay behind each door. The mere thought that his decisions would mean something was almost too wonderful to behold!")
+                    print("As he wandered through this fantasy world, he began to fill it with many possible paths and destinations.")
+                    y=random.choice(string.ascii_lowercase)
+                    print("Press the " +y+ " key.")
+                    x=input()
+                    if x == y:
+                        import random
+                        import string
+                        random.choice(string.ascii_lowercase)
+                        print("Down one path lay an enormous round room with monitors and mind controls, and down another was a yellow line that weaved in many directions, and down another was a game with a baby. And he called it;")
+                        print("The Stanley Parable.")
+                        y=random.choice(string.ascii_lowercase)
+                        print("Press the " +y+ " key.")
+                        x=input()
+                        if x == y:
+                            import random
+                            import string
+                            random.choice(string.ascii_lowercase)
+                            print("It was such a wonderful fantasy, and so in his head he relived it again. And then again, and again, over and over, wishing beyond hope that it would never end, that he might always feel this free.")
+                            print("Surely, there's an answer down some new path! Mustn't there be? Perhaps if he played just one more time....")
+                            y=random.choice(string.ascii_lowercase)
+                            print("Press the " +y+ " key.")
+                            x=input()
+                            if x == y:
+                                import random
+                                import string
+                                random.choice(string.ascii_lowercase)
+                                print("But there is no answer. How could there possibly be?")
+                                print("In reality, all he's doing is pushing the same buttons he always has, nothing has changed. The longer he spends here, the more invested he gets, the more he forgets which life is the real one.")
+                                y=random.choice(string.ascii_lowercase)
+                                print("Press the " +y+ " key.")
+                                x=input()
+                                if x == y:
+                                    import random
+                                    import string
+                                    random.choice(string.ascii_lowercase)
+                                    print("And I'm trying to tell him this, that in this world, he can never be anything but an observer, that as long as he remains here, he's slowly killing himself. But he won't listen to me. He won't stop!")
+                                    print("Here, watch this. Stanley, the next time the screen asks you to push a button, do not do it!")
+                                    y=random.choice(string.ascii_lowercase)
+                                    print("Press the " +y+ " key.")
+                                    x=input()
+                                    if x == y:
+                                        import random
+                                        import string
+                                        random.choice(string.ascii_lowercase)
+                                        print("You see? Can he just not hear me? How can I tell him in a way that he'll understand, that every second he remains here, he's electing to kill himself?")
+                                        print("How can I get him to see what I see? How can I make him look at himself?")
+                                        y=random.choice(string.ascii_lowercase)
+                                        print("Press the " +y+ " key.")
+                                        x=input()
+                                        if x == y:
+                                            import random
+                                            import string
+                                            random.choice(string.ascii_lowercase)
+                                            print("I suppose I can't, not in the way I want him to.")
+                                            print("But I don't make the rules, I simply play to my intended purpose, the same as Stanley. We're not so different, I suppose. I'll try once more to convey all this to him. I'm compelled to. I must.")
+                                            print("Perhaps... well, maybe this time he'll see. Maybe this time.")
+                                            y=random.choice(string.ascii_lowercase)
+                                            print("Press the " +y+ " key.")
+                                            x=input()
+                                            if x == y:
+                                                print("And I tried again. And Stanley pushed a button. And I tried again. And Stanley pushed a button.")
+                                                print("GAME OVER")
+                                                print("Input anything to restart.")
+                                                input()
+                                            else:
+                                                 print("You are fired employee #427.")
+                                                 print("YOU WIN!")
+                                                 print("The goal was to free Stanley, which you did!")
+                                                 print("Input anything to restart.")
+                                                 input()
+                                        else:
+                                             print("You are fired employee #427.")
+                                             print("YOU WIN!")
+                                             print("The goal was to free Stanley, which you did!")
+                                             print("Input anything to restart.")
+                                             input()
+                                    else:
+                                         print("You are fired employee #427.")
+                                         print("YOU WIN!")
+                                         print("The goal was to free Stanley, which you did!")
+                                         print("Input anything to restart.")
+                                         input()
+                                else:
+                                     print("You are fired employee #427.")
+                                     print("YOU WIN!")
+                                     print("The goal was to free Stanley, which you did!")
+                                     print("Input anything to restart.")
+                                     input()
+                            else:
+                                 print("You are fired employee #427.")
+                                 print("YOU WIN!")
+                                 print("The goal was to free Stanley, which you did!")
+                                 print("Input anything to restart.")
+                                 input()
+                        else:
+                             print("You are fired employee #427.")
+                             print("YOU WIN!")
+                             print("The goal was to free Stanley, which you did!")
+                             print("Input anything to restart.")
+                             input()
+                    else:
+                         print("You are fired employee #427.")
+                         print("YOU WIN!")
+                         print("The goal was to free Stanley, which you did!")
+                         print("Input anything to restart.")
+                         input()
+                else:
+                     print("You are fired employee #427.")
+                     print("YOU WIN!")
+                     print("The goal was to free Stanley, which you did!")
+                     print("Input anything to restart.")
+                     input()
+            else:
+                 print("You are fired employee #427.")
+                 print("YOU WIN!")
+                 print("The goal was to free Stanley, which you did!")
+                 print("Input anything to restart.")
+                 input()
+        else:
+             print("You are fired employee #427.")
+             print("YOU WIN!")
+             print("The goal was to free Stanley, which you did!")
+             print("Input anything to restart.")
+             input()
+    else:
+        print("You are fired employee #427.")
+        print("YOU WIN!")
+        print("The goal was to free Stanley, which you did!")
+        print("Input anything to restart.")
+        input()
